23672ab1bec22fe37d78ba162040ae3764953922
AythaE <aythae@gmail.com>
2017-05-19 18:46:17 +0200

    Comenzando la autenticación #9

---
+++ T6/app/restaurantes/views.py
@@ -0,0 +1,105 @@
+# -*- coding: utf-8 -*-
+from django.shortcuts import HttpResponse, redirect, render
+
+from .forms import RestaurantesForm
+# El punto se refiere al directorio de este archivo
+from .models import addr, image, restaurants
+
+
+# Create your views here.
+
+# 2 Funcuones correspondientes al enrutador "urls.py"
+
+
+def index(request):
+  restaurantes = restaurants.objects
+  ultimosRestaurants = list(reversed(
+      restaurantes[len(restaurantes) - 10:len(restaurantes)]))
+  context = {
+      'resta': ultimosRestaurants,
+  }
+  return render(request, 'home.html', context)
+
+
+def test(request):
+
+  context = {
+      'variable': 3,
+      'resta': restaurants.objects(cuisine='Granaina'),
+  }   # Aquí van la las variables para la plantilla
+  return render(request, 'test.html', context)
+
+
+def buscar(request):
+  cocina = request.GET.get('cocina')
+  lista = restaurants.objects(cuisine__icontains=cocina)
+
+  context = {
+      'resta': lista,
+      'busqueda': cocina,
+  }
+  return render(request, 'buscar.html', context)
+
+
+def add(request):
+  formu = RestaurantesForm()
+
+  if request.method == "POST":
+
+    formu = RestaurantesForm(data=request.POST)
+    if formu.is_valid():                    # valida o añade errores
+      print(formu.cleaned_data)
+      # datos sueltos
+
+      nombre = formu.cleaned_data['nombre']
+      cocina = formu.cleaned_data['cocina']
+      barrio = formu.cleaned_data['barrio']
+      calle = formu.cleaned_data['dirección']
+
+      # Revisar si esto funciona así
+      imagen = request.FILES.get('imagen')
+      tipo_foto = imagen.content_type
+      # Para guardar tipo de archivo y nombre
+      direc = addr(street=calle)
+      i = image(extension=tipo_foto, img=imagen)
+      r = restaurants(name=nombre, cuisine=cocina,
+                      borough=barrio, address=direc, image=i)
+
+      r.save()
+
+      # formu.save()                         # si está ligado al model
+
+      return redirect(index)
+  # GET o error
+  context = {
+      'form': formu,
+  }
+  return render(request, 'aniadir.html', context)
+
+# Sacar campo name poniendo () en url.py
+
+
+def restaurante(request, name):
+  print("\n\nMostrando restaurante: " + name)
+  resta = restaurants.objects(name__exact=name)[0]
+
+  print(resta)
+  context = {
+      'resta': resta,
+  }
+  return render(request, 'restaurante.html', context)
+
+
+# /restaurante/nombreRest/imagen/
+
+
+def imagen(request, name):
+
+  res = restaurants.objects(name__exact=name)[0]
+
+  print("\n\n recuperando imagen de: " + res.name)
+  img = res.image.img.read()
+
+  content_type = res.image.extension
+
+  return HttpResponse(img, content_type=content_type)

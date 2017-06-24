97861af3813ce622c6070cbe02df3817d17a486a
AythaE <aythae@gmail.com>
2017-06-24 20:41:04 +0200

    Tarea 8_bis

---
+++ T8_bis/app/app/urls.py
@@ -0,0 +1,42 @@
+# -*- coding: utf-8 -*-
+"""app URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/1.10/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  url(r'^$', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  url(r'^$', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.conf.urls import url, include
+    2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
+"""
+from django.conf.urls import include, url
+from django.contrib import admin
+from django.views.decorators.csrf import csrf_exempt
+from registration.backends.simple.views import RegistrationView
+from rest_framework.authtoken.views import obtain_auth_token
+from rest_framework.routers import DefaultRouter
+
+
+class MyRegistrationView(RegistrationView):
+
+  def get_success_url(self, user):
+    return '/restaurantes/'
+
+
+router = DefaultRouter()
+
+
+urlpatterns = [
+    url(r'^restaurantes/', include('restaurantes.urls')),
+    url(r'^admin/', admin.site.urls),
+    url(r'^accounts/register/$', MyRegistrationView.as_view(),
+        name='registration_register'),
+    url(r'^accounts/', include('registration.backends.simple.urls')),
+    url(r'^obtain-auth-token/$', csrf_exempt(obtain_auth_token)),
+
+]
